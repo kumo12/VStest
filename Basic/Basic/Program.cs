07ab5183e2cda0315d03e1edd8b3eaa7951d502b
30302 고유빈 <63216449+kumo12@users.noreply.github.com>
2020-08-13 16:11:44 +0900

    Add files via upload

---
+++ Basic/Basic/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClass
+{
+    class Cat
+    {
+        public string Name;
+        public string color;
+
+        public Cat()
+        {
+            Name = "";
+            color = "";
+        }
+
+        public Cat(string _Name, string _Color)
+        {
+            Name = _Name;
+            color = _Color;
+        }
+
+        ~Cat()
+        {
+            Console.WriteLine($"{Name} : 잘가");
+        }
+        public void Meow()
+        {
+            Console.WriteLine($"{Name} : 야옹");
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Cat kitty = new Cat();
+            kitty.color = "하얀색";
+            kitty.Name = "키티";
+            kitty.Meow();
+            Console.WriteLine($"{kitty.Name} : {kitty.color}");
+
+            Cat nero = new Cat("네로", "검은색");
+            nero.Meow();
+            Console.WriteLine($"{nero.Name} : {nero.color}");
+        }
+    }
+}
