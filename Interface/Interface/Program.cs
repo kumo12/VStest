a728522f302fd54a2dec0d69a23bfe223fb1d444
30302 고유빈 <63216449+kumo12@users.noreply.github.com>
2020-08-14 14:31:53 +0900

    Add files via upload

---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Interface
+{
+    interface ILogger
+    {
+        void WriteLog(string message);
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        public void WriteLog(string message)
+        {
+            Console.WriteLine(
+                "{0} {1}",
+                DateTime.Now.ToLocalTime(), message);
+        }
+    }
+
+
+        class FileLogger : ILogger
+        {
+        private StreamWriter Writer;
+            public FileLogger(string path)
+            {
+                Writer = File.CreateText(path);
+                Writer.AutoFlush = true;
+            }
+
+            public void WriteLog(string message)
+            {
+                Writer.WriteLine("{0} {1}", DateTime.Now.ToShortDateString(), message);
+            }
+        }
+
+        class ClimateMonitor
+        {
+            private ILogger Iogger;
+            public ClimateMonitor(ILogger logger)
+            {
+                this.Iogger = logger;
+            }
+
+            public void start()
+            {
+                while (true)
+                {
+                    Console.WriteLine("온도를 입력해주세요.: ");
+                    string temperature = Console.ReadLine();
+                    if (temperature == " ")
+                        break;
+
+                    Iogger.WriteLog("현재 온도 : " + temperature);
+                }
+            }
+        }
+
+        class MainApp
+        {
+            static void Main(string[] args)
+            {
+                ClimateMonitor monitor = new ClimateMonitor(
+                    new FileLogger("MyLog.txt"));
+
+                monitor.start();
+            }
+        }
+    }
---
+++ Overriding/Overriding/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overriding
+{
+    class ArmorSuite
+    {
+        public virtual void Initialize()
+        {
+            Console.WriteLine("Armored");
+        }
+    }
+
+    class IronMan : ArmorSuite
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Console.WriteLine("Repulsor Rays Ared");
+        }
+    }
+
+    class WarMachine : ArmorSuite
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Console.WriteLine("Double-Barrel Cannons Armed");
+            Console.WriteLine("Micro-Rocket Launcher Ardm");
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating ArmorSuite...");
+            ArmorSuite armorsuite = new ArmorSuite();
+            armorsuite.Initialize();
+
+            Console.WriteLine("\nCreating IronMan...");
+            ArmorSuite ironman = new IronMan();
+            ironman.Initialize();
+
+            Console.WriteLine("\nCreating WarMachine...");
+            ArmorSuite warmachine = new WarMachine();
+            ironman.Initialize();
+        }
+    }
+}
