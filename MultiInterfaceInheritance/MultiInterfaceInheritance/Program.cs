a810469d714a6d313791ceb7eb2e5decf8f90edd
30302 고유빈 <63216449+kumo12@users.noreply.github.com>
2020-09-07 00:42:54 +0900

    Add files via upload

---
+++ MultiInterfaceInheritance/MultiInterfaceInheritance/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MultiInterfaceInheritance
+{
+    interface IRunnable
+    {
+        void Run();
+    }
+    interface IFlyable
+    {
+        void Fly();
+    }
+
+    //다중상속
+    class FlyingCar : IRunnable, IFlyable
+    {
+        public void Run()
+        {
+            Console.WriteLine("Run! Run!");
+        }
+        public void Fly()
+        {
+            Console.WriteLine("Fly! Fly!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FlyingCar car = new FlyingCar();
+            car.Run();
+            car.Fly();
+
+            //IFlyable flyable = new FlyingCar(); as IFlyable;
+            IRunnable runnable = car as IRunnable;
+            runnable.Run();
+
+            IFlyable flyable = car as IFlyable;
+            flyable.Fly();
+
+        }
+    }
+}
