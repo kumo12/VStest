ab528a3e6b9115edc658e65bbd729aa1574722ce
30302 고유빈 <63216449+kumo12@users.noreply.github.com>
2020-09-11 16:03:39 +0900

    Add files via upload

---
+++ LoginApp/LoginApp/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace LoginApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlcon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\user\source\repos\LoginApp\LoginApp\Database1.mdf;Integrated Security=True");
+            string query = "SELECT * FROM login WHERE Username='"+textBox1.Text.Trim()+"'" +
+                           "and Password='"+textBox2.Text.Trim()+"'";
+            SqlDataAdapter sda = new SqlDataAdapter(query, sqlcon);
+            DataTable dtbl = new DataTable();
+            sda.Fill(dtbl);
+            if(dtbl.Rows.Count == 1)
+            {
+                Form2 form2 = new Form2();
+                this.Hide();
+                form2.Show();
+            }
+            else
+            {
+                MessageBox.Show("이름이나 비번을 체크하세요.");
+            }
+        }
+    }
+}
---
+++ SQLQuery2.sql
@@ -0,0 +1,8 @@
+﻿select * from Student;
+select Id, Name from Student
+where Score > 50;
+
+insert into Student
+values(30111, 'Soo', '010-2345-5678', 100);
+
+select * from Student;
